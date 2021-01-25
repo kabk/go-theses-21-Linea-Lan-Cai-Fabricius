7c2eb3fcb8af6b6f00ef6b3a03a4ec034f7921b0
lineafab <lineafab93@hotmail.com>
2021-01-25 17:50:40 +0100

    hover

--- script.js
+++ script.js
@@ -123,3 +123,25 @@ $(document).ready(function(){
 //   }
 // })
 // }
+
+const sounds = ["Solen er så rød mor"];
+const buttons = document.getElementById("buttons");
+
+const stopSounds = () => {
+  sounds.forEach((sound) => {
+    const currentSound = document.getElementById(sound);
+    currentSound.pause();
+    currentSound.currentTime = 0;
+  });
+};
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+  btn.addEventListener("click", () => {
+    stopSounds();
+    document.getElementById(sound).play();
+  });
+  buttons.appendChild(btn);
+});
